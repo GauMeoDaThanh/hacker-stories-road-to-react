4478e57f09a918c954b7c0edba3284ca37407c76
Van Dat <ddat888@gmail.com>
2024-04-11 19:48:21 +0700

    Learn: Arrow Function

--- src/App.jsx
+++ src/App.jsx
@@ -20,29 +20,34 @@ const list = [
   },
 ];
 
-function App() {
-  return (
-    <div>
-      <h1>My Hacker Stories</h1>
-      <label htmlFor="search">Search:</label>
-      <input type="text" id="text" />
-      <hr />
-      <ul>
-        {list.map(function (item) {
-          return (
-            <li key={item.objectID}>
-              <span>
-                <a href={item.url}>{item.title}</a>
-              </span>
-              <span>{item.author}</span>
-              <span>{item.num_comments}</span>
-              <span>{item.points}</span>
-            </li>
-          );
-        })}
-      </ul>
-    </div>
-  );
-}
+const App = () => (
+  <div>
+    <h1>My Hacker Stories</h1>
+    <Search />
+    <hr />
+    <List />
+  </div>
+);
 
+const List = () => (
+  <ul>
+    {list.map((item) => (
+      <li key={item.objectID}>
+        <span>
+          <a href={item.url}>{item.title}</a>
+        </span>
+        <span>{item.author}</span>
+        <span>{item.num_comments}</span>
+        <span>{item.points}</span>
+      </li>
+    ))}
+  </ul>
+);
+
+const Search = () => (
+  <div>
+    <label htmlFor="search">Search:</label>
+    <input type="text" id="text" />
+  </div>
+);
 export default App;
