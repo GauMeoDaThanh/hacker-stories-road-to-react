90165ca7cf33007fdedb299a718acd67f075f494
Van Dat <ddat888@gmail.com>
2024-04-11 21:32:46 +0700

    Learn: Nested Destructuring

--- src/App.jsx
+++ src/App.jsx
@@ -53,16 +53,18 @@ const List = ({ list }) => {
   );
 };
 
-const Item = ({ item }) => (
-  <li>
-    <span>
-      <a href={item.url}>{item.title}</a>
-    </span>
-    <span>{item.author}</span>
-    <span>{item.num_comments}</span>
-    <span>{item.points}</span>
-  </li>
-);
+const Item = ({ item: { title, url, author, num_comments, points } }) => {
+  return (
+    <li>
+      <span>
+        <a href={url}>{title}</a>
+      </span>
+      <span>{author}</span>
+      <span>{num_comments}</span>
+      <span>{points}</span>
+    </li>
+  );
+};
 
 const Search = ({ search, onSearch }) => (
   <div>
