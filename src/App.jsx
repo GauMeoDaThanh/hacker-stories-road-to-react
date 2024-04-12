ba73b1d1e693341f426218167309ce38d13240fe
Van Dat <ddat888@gmail.com>
2024-04-12 10:07:40 +0700

    Learn: React Fragments

--- src/App.jsx
+++ src/App.jsx
@@ -77,10 +77,10 @@ const Item = ({ item }) => {
 };
 
 const Search = ({ search, onSearch }) => (
-  <div>
+  <React.Fragment>
     <label htmlFor="search">Search:</label>
     <input type="text" id="search" onChange={onSearch} value={search} />
-  </div>
+  </React.Fragment>
 );
 
 export default App;
